c308ae1bde8e93a7f9fb3c78fbde6ac226aeead0
Matthias <Matt.Kanke@gmail.com>
2017-03-11 16:52:00 +0000

    Detects motion in room, and tracks moving objects. Thresholding is sensitive

--- cat_regulation.py
+++ cat_regulation.py
@@ -3,6 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import datetime
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -24,41 +25,56 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     # if the first frame is None, initialize it
-#    if firstFrame is None:
-#        firstFrame = gray
-#        continue
- 
+    if firstFrame is None:
+        firstFrame = gray
+        rawCapture.truncate(0)
+        time.sleep(1)
+        continue
+
+    # Set text
+    text = 'Unoccupied'
+
     # compute the absolute difference between the current frame and
     # first frame
-#    frameDelta = cv2.absdiff(firstFrame, gray)
-#    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 100, 255, cv2.THRESH_BINARY)[1]
 
     # dilate the thresholded image to fill in holes, then find contours
     # on thresholded image
-#    thresh = cv2.dilate(thresh, None, iterations=2)
-#    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, 
-#                                 cv2.CHAIN_APPROX_SIMPLE)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # loop over the contours
-#    for c in cnts:
+    for c in cnts:
         # if the contour is too small, ignore it
-#        if cv2.contourArea(c) < args["min_area"]:
-#            continue
+        if cv2.contourArea(c) < 500:
+            continue
 
         # compute the bounding box for the contour, draw it on the frame,
         # and update the text
-#        (x, y, w, h) = cv2.boundingRect(c)
-#        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#        text = "Occupied"
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Occupied"
 
 # draw the text and timestamp on the frame
-#    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-#    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-#    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-#               (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    cv2.putText(image, 
+                "Room Status: {}".format(text), 
+                (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                0.75, 
+                (0, 255, 0), 
+                1)
+    cv2.putText(image, 
+                datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), 
+                (10, 40), 
+                cv2.FONT_HERSHEY_TRIPLEX, 
+                0.5, 
+                (0, 0, 255), 
+                1)
 
 	# show the frame
-    cv2.imshow("Frame", gray)
+    cv2.imshow("Frame", image)
+    cv2.imshow("Thresh", thresh)
     key = cv2.waitKey(1) & 0xFF
  
 	# clear the stream in preparation for the next frame
